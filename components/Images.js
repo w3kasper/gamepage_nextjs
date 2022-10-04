bd010f02c18bd99d173e442dc4fdc4e721384134
w3kasper <108923998+w3kasper@users.noreply.github.com>
2022-10-03 23:40:46 -0600

    update

--- components/Images.js
+++ components/Images.js
@@ -1,16 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 import Image from "next/image";
-import imageWheel from "../public/images/1.png";
-import { SliderData } from "./Sliderdata";
+import imageOne from "../public/images/1.png";
+import imageTwo from "../public/images/2.png";
+import imageThree from "../public/images/3.png";
+//https://youtu.be/ho93e0IhdTA?t=606
+const featuredImages = [imageOne, imageTwo, imageThree];
+let count = 0;
 
 const ImageSlider = () => {
-  //embla carousel
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  const handleOnNextClick = () => {
+    count = (count + 1) % featuredImages.length;
+    setCurrentIndex(count);
+  };
+
+  const handleOnPrevClick = () => {
+    const imagesLength = featuredImages.length;
+
+    count = (currentIndex + imagesLength - 1) % imagesLength;
+    setCurrentIndex(count);
+  };
+
   return (
-    <>
-      {SliderData.map((slide, index) => {
-        return <img src={slide.image} alt="travel image" />;
-      })}
-    </>
+    <div className="select-none">
+      <Image
+        src={featuredImages[currentIndex]}
+        className="aspect=w-16 aspect-h-9"
+        alt="/"
+      />
+      <div className="absolute w-[990px] top-1/2 transform -translate-y-1/2 flex justify-between items-center">
+        <button onClick={handleOnPrevClick}>Previous</button>
+        <button onClick={handleOnNextClick}>Next</button>
+      </div>
+    </div>
   );
 };
 
