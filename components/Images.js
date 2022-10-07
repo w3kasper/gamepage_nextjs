274e008eb0258d87e8d8a15033e6064daba9d500
w3kasper <108923998+w3kasper@users.noreply.github.com>
2022-10-06 20:55:58 -0600

    update youtube player

--- components/Images.js
+++ components/Images.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Image from "next/image";
 import imageOne from "../public/images/1.png";
 import imageTwo from "../public/images/4.png";
@@ -7,31 +7,61 @@ import imageThree from "../public/images/3.png";
 import nextButton from "../public/images/next.svg";
 import prevButton from "../public/images/prev.svg";
 
+import YoutubeEmbed from "./YoutubeEmbed";
+
 const featuredImages = [imageOne, imageTwo, imageThree];
 let count = 0;
 
 const ImageSlider = () => {
   const [currentIndex, setCurrentIndex] = useState(0);
+  const [imageType, setImageType] = useState(true);
+
+  useEffect(() => {
+    document.title = `index: ${currentIndex} it: ${imageType}`;
+  }, [currentIndex]);
 
   const handleOnNextClick = () => {
     count = (count + 1) % featuredImages.length;
     setCurrentIndex(count);
+
+    if (count == 0) {
+      setImageType(true);
+    } else {
+      setImageType(false);
+    }
   };
 
   const handleOnPrevClick = () => {
     const imagesLength = featuredImages.length;
-
     count = (currentIndex + imagesLength - 1) % imagesLength;
     setCurrentIndex(count);
+
+    if (count == 0) {
+      setImageType(true);
+    } else {
+      setImageType(false);
+    }
   };
 
-  return (
-    <div className="select-none">
+  const renderImage = () => (
+    <div className="text white">
       <Image
         src={featuredImages[currentIndex]}
         className="aspect=w-16 aspect-h-9"
         alt="/"
       />
+    </div>
+  );
+
+  const renderYoutube = () => (
+    <div className="text white">
+      <YoutubeEmbed embedId="rokGy0huYEA" />
+    </div>
+  );
+
+  return (
+    <div className="select-none">
+      <div>{imageType ? renderYoutube() : renderImage()}</div>
       <div className="absolute w-[990px] top-1/2 transform translate-y-[60px] flex justify-between items-center px-[10px]">
         <button onClick={handleOnPrevClick}>
           <Image
--- pages/index.js
+++ pages/index.js
@@ -22,7 +22,7 @@ const App = () => {
         <div class="place-content-center">
           <Navbar />
         </div>
-        <div class="w-full h-[90px]"></div>
+        <div class="w-full h-[100px]"></div>
         <div class="flex justify-center">
           <div class="bg-black border border-pink-500 rounded-lg grid grid-cols-3 grid-rows-9 gap-2 max-w-5xl h-auto justify-items-center p-[15px]">
             <div class="box col-start-1 col-end-5">
