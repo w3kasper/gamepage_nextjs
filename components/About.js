0d229b06f440319ae284b5936f4c27e0045f29a4
w3kasper <108923998+w3kasper@users.noreply.github.com>
2022-10-10 01:42:20 -0600

    update about.js

--- components/About.js
+++ components/About.js
@@ -4,34 +4,35 @@ import aboutTitle1 from "../public/images/about/title.png";
 import aboutTitle2 from "../public/images/about/title2.png";
 import aboutTitle3 from "../public/images/about/title3.png";
 
-import YoutubeEmbed from "./YoutubeEmbed";
 import solLogo from "../public/images/sol-logo.svg";
 
 const About = () => {
   return (
     <>
-      <div className="bg-neutral-900 border border-purple-500 rounded-lg p-[15px] font-sans font-medium text-xl text-white uppercase">
-        <div>
-          <span>Buy Cops Kissing Each Other</span>{" "}
-          <span className="pl-[350px]">OO</span>
+      <div className="grid overflow-hidden grid-cols-8 grid-rows-1 gap-0 w-[735px] bg-neutral-900 border border-purple-500 rounded-lg p-[8px] text-white uppercase">
+        <div className="box col-start-1 col-end-6">
+          <div className="pt-[3px] pl-[12px] font-sans font-light text-2xl">
+            Cops Kissing Each Other
+          </div>
         </div>
-      </div>
-      <div className="pl-[550px]">
-        <div className="flex space-x-3 border-purple-500 bg-neutral-900 -top-3">
-          {" "}
-          <Image
-            src={solLogo}
-            className=""
-            height={26}
-            width={26}
-            alt="/"
-          />{" "}
-          <text className="font-sans font-normal text-white text-1xl text-center">
-            0.75
-          </text>
-          <button className="w-[80px] h-[24px] bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 hover:from-indigo-900 hover:via-purple-900 hover:to-pink-900 outline outline-1 rounded-full font-sans font-thin text-sm text-white text-center">
-            BUY
-          </button>
+        <div className="box col-start-6 col-end-7 text-center pt-2">WIN</div>
+        <div className="box col-start-7 col-end-9">
+          <div className="flex items-center space-x-3 rounded-full bg-neutral-800 p-2 pl-[15px]">
+            {" "}
+            <Image
+              src={solLogo}
+              className=""
+              height={24}
+              width={24}
+              alt="/"
+            />{" "}
+            <text className="font-sans font-normal text-white text-1xl text-center">
+              0.75
+            </text>
+            <button className="w-[80px] h-[24px] bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 hover:from-indigo-900 hover:via-purple-900 hover:to-pink-900 outline outline-1 rounded-full font-sans font-thin text-sm text-white text-center">
+              BUY
+            </button>
+          </div>
         </div>
       </div>
 
