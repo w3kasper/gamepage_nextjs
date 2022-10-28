be4e1a958e567798d1ae33fcf9141dcdcf8c2174
w3kasper <108923998+w3kasper@users.noreply.github.com>
2022-10-28 00:41:14 -0600

    update

--- components/Download.js
+++ components/Download.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import Product from "./Product";
 import IPFSDownload from "./IpfsDownload";
+import IPFSUnconnected from "./IPFSUnconnected";
 
 import { useWallet } from "@solana/wallet-adapter-react";
 import { WalletMultiButton } from "@solana/wallet-adapter-react-ui";
@@ -9,40 +10,55 @@ const Download = () => {
   const { publicKey } = useWallet();
   const [products, setProducts] = useState([]);
 
+  useEffect(() => {
+    if (publicKey) {
+      fetch(`/api/fetchProducts`)
+        .then((response) => response.json())
+        .then((data) => {
+          setProducts(data);
+          console.log("Products", data);
+        });
+    }
+  }, [publicKey]);
+
   const renderNotConnectedContainer = () => (
-    <div>
-      <p className="text-white">NOT CONNECTED SELECT WALLET</p>
-    </div>
-  );
-
-  const renderItemBuyContainer = () => (
-    <div className="text-white">
-      <p className="text-white">CONNECTED</p>
-
-      <IPFSDownload
-        filename="games.zip"
-        hash="QmdabcZ2xS72ywHnxuVGnAcR4nzyZnXfNFumpZzSK17GT3"
-        cta="Download games"
-      />
-    </div>
-  );
-
-  return (
-    <div className="bg-neutral-900 border border-purple-500 rounded-lg p-3 pt-4">
+    <>
       <div className="px-[6px] pb-3 space-x-3">
-        <button
-          disabled
-          className="w-[80px] h-[30px] bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 hover:from-indigo-900 hover:via-purple-900 hover:to-pink-900 outline outline-1 rounded font-sans font-thin text-sm text-white text-center uppercase disabled:opacity-30 disabled:hover:from-indigo-500 disabled:hover:via-purple-500 disabled:hover:to-pink-500"
-        >
-          PLAY
-        </button>
-        <button className="w-[80px] h-[30px] bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 hover:from-indigo-900 hover:via-purple-900 hover:to-pink-900 outline outline-1 rounded font-sans font-thin text-sm text-white text-center uppercase">
-          DEMO
-        </button>
+        <IPFSUnconnected
+          demohash="QmdabcZ2xS72ywHnxuVGnAcR4nzyZnXfNFumpZzSK17GT3"
+          demofile="demo"
+        />
       </div>
       <div className="text-white text-centerfont-sans font-thin text-xs text-center">
         -downloadable .zip file-
       </div>
+      <div>
+        <p className="text-white">NOT CONNECTED SELECT WALLET</p>
+      </div>
+    </>
+  );
+
+  const renderItemBuyContainer = () => (
+    <>
+      <div className="px-[6px] pb-3 space-x-3">
+        <IPFSDownload
+          demohash="QmdabcZ2xS72ywHnxuVGnAcR4nzyZnXfNFumpZzSK17GT3"
+          demofile="demo"
+          filenamefull="game"
+          hashfull="Qmf1k8hEFFfFFEehXLfJoCBLCs2WhT3CtYS2D94z5XfiDV"
+        />
+      </div>
+      <div className="text-white text-centerfont-sans font-thin text-xs text-center">
+        -downloadable .zip file-
+      </div>
+      <div className="text-white">
+        <p className="text-white">CONNECTED</p>
+      </div>
+    </>
+  );
+
+  return (
+    <div className="bg-neutral-900 border border-purple-500 rounded-lg p-3 pt-4">
       <main>
         {publicKey ? renderItemBuyContainer() : renderNotConnectedContainer()}
       </main>
--- components/IPFSUnconnected.js
+++ components/IPFSUnconnected.js
@@ -1,18 +1,16 @@
 import React from "react";
 import useIPFS from "../hooks/useIPFS";
 
-const IPFSDownload = ({ demohash, demofile, hashfull, filenamefull }) => {
+const IPFSUnconnected = ({ demohash, demofile }) => {
   const file = useIPFS(demohash, demofile);
-  const filefull = useIPFS(hashfull, filenamefull);
 
   return (
     <div>
       {file ? (
         <div className="space-x-3">
           <a
-            className="py-[6px] px-[26px] w-[80px] h-[30px] bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 hover:from-indigo-900 hover:via-purple-900 hover:to-pink-900 outline outline-1 rounded font-sans font-thin text-sm text-white text-center uppercase"
-            href={filefull}
-            download="game"
+            className="py-[6px] px-[26px] w-[80px] h-[30px] bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 hover:from-indigo-500 hover:via-purple-500 hover:to-pink-500 outline outline-1 rounded font-sans font-thin text-sm text-white text-center uppercase opacity-30"
+            disabled
           >
             PLAY
           </a>
@@ -31,4 +29,4 @@ const IPFSDownload = ({ demohash, demofile, hashfull, filenamefull }) => {
   );
 };
 
-export default IPFSDownload;
+export default IPFSUnconnected;
