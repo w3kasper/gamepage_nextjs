85c56914e5c135861c7d6a897ff78c2ce1695d6a
w3kasper <108923998+w3kasper@users.noreply.github.com>
2022-11-27 00:46:43 -0700

    Update Download.js

--- components/Download.js
+++ components/Download.js
@@ -34,12 +34,13 @@ const Download = () => {
 
   //if public key exists setaddress to it on refresh/reload of publicKey when its updated
   useEffect(() => {
-    publicKey ? setAddress(publicKey.toBase58()) : "NOT CONNECTED/NULL";
+    publicKey ? setAddress(publicKey.toBase58()) : "NULL";
   }, [publicKey]);
 
   //console.log(address.toString()); //this is the address
 
   const [nftList, setNftList] = useState(null); //used to look at list
+  const [holder, setHolder] = useState(null); //used to look at list
 
   ///ON LOAD NFT LIST
   // declare the data fetching function
@@ -56,6 +57,11 @@ const Download = () => {
         const listItems = list.map((nftlist) => {
           //console.log(nftlist.updateAuthorityAddress.toBase58());
           console.log(nftlist.name);
+          if (nftlist.name == "Vaxxed Doggo #1169") {
+            console.log("set a useeffect to load ");
+            setHolder("HiHolder"); //is holding
+            console.log(holder);
+          }
         });
 
         //.updateAuthorityAddress.toBase58()
@@ -139,6 +145,7 @@ const Download = () => {
     </>
   );
 
+  /*
   const renderHolderContainer = () => (
     <>
       <div classNames="grid grid-cols-2 grid-rows-1 gap-0 ">
@@ -163,10 +170,29 @@ const Download = () => {
       </div>
     </>
   );
+  */
 
-  const renderNonholderContainer = () => (
+  const renderConnectedContainer = () => (
     <>
       <div className="grid grid-cols-2 grid-rows-1 gap-0 ">
+        {holder ? (
+          <div className="box">
+            <Playbutton
+              filenamefull="game"
+              hashfull="Qmf1k8hEFFfFFEehXLfJoCBLCs2WhT3CtYS2D94z5XfiDV"
+            />
+          </div>
+        ) : (
+          <div className="box">
+            <a
+              className="py-[6px] px-[26px] w-[80px] h-[30px] bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 hover:from-indigo-500 hover:via-purple-500 hover:to-pink-500 outline outline-1 rounded font-sans font-thin text-sm text-white text-center uppercase opacity-30"
+              disabled
+            >
+              PLAY
+            </a>
+          </div>
+        )}
+        {/*
         <div className="box">
           <a
             className="py-[6px] px-[26px] w-[80px] h-[30px] bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 hover:from-indigo-500 hover:via-purple-500 hover:to-pink-500 outline outline-1 rounded font-sans font-thin text-sm text-white text-center uppercase opacity-30"
@@ -175,6 +201,7 @@ const Download = () => {
             PLAY
           </a>
         </div>
+        */}
         <div className="box">
           <Demobutton
             demohash="QmdabcZ2xS72ywHnxuVGnAcR4nzyZnXfNFumpZzSK17GT3"
@@ -187,7 +214,7 @@ const Download = () => {
         -downloadable .zip file-
       </div>
       <div className="text-white text-centerfont-sans font-thin text-xs text-center pt-[10px]">
-        Status: CONNECTED + NONHOLDER+
+        Status: CONNECTED + {holder ? "HOLDER" : "NON HOLDER"}
       </div>
       {/* 
       <button className="text-white" onClick={fetchNFTs}>
@@ -200,7 +227,7 @@ const Download = () => {
   return (
     <div className="bg-neutral-900 border border-purple-500 rounded-lg p-3 pt-4">
       <main>
-        {publicKey ? renderNonholderContainer() : renderNotConnectedContainer()}
+        {publicKey ? renderConnectedContainer() : renderNotConnectedContainer()}
       </main>
     </div>
   );
